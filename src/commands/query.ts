5a2a9c68182cbdb9e5ab135e179e07db1f8c6e55
Ladybunne <github@ladybunne.net>
2023-02-14 13:47:13 +1100

    Remove some old commands

--- src/commands/query.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction, InteractionResponse, ChatInputCommandInteraction } from "discord.js";
-
-export const data: SlashCommandBuilder = new SlashCommandBuilder()
-	.setName('query')
-	.setDescription('Query the rabbit.');
-
-export async function execute(interaction: ChatInputCommandInteraction) {
-	await interaction.reply({ content: 'Querying the rabbit is not yet implemented.', ephemeral: true });
-}
--- src/commands/rabbit.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction, InteractionResponse, ChatInputCommandInteraction } from "discord.js";
-
-export const data: SlashCommandBuilder = new SlashCommandBuilder()
-	.setName('rabbit')
-	.setDescription('🐰');
-
-export async function execute(interaction: ChatInputCommandInteraction) {
-	await interaction.reply({ content: '🐰' });
-}
