09ea7ecf2cdd9e2411d98b750914bf45bc86d339
Ladybunne <github@ladybunne.net>
2023-03-18 23:18:45 +1100

    Swap over to prod config

--- up.sh
+++ up.sh
@@ -1,13 +1,11 @@
 # RivalBot Restart Script - up.sh
 # Author: Ladybunne
 # Purpose: Arrange all config files correctly, then bring RivalBot back up!
-#
-# Intended to be run as: nohup up.sh &
 # ---
 
 # Move files from credentials/ to build/
 cp ~/RivalBot/credentials/.env ~/RivalBot/build/.env
-cp ~/RivalBot/credentials/rivalbot-config-dev.json ~/RivalBot/build/src/configs/rivalbot-config.json
+cp ~/RivalBot/credentials/rivalbot-config-prod.json ~/RivalBot/build/src/configs/rivalbot-config.json
 
 # Move to build/ and run npm start
 # ...is what I'd like to say, but it's fucky.
